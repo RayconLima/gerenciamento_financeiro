0763b65deff6f0213b68fab0ec2746bf0b9abda5
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2025-10-08 01:03:44 -0400

    adicionar front

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsuarioSeeder::class,
-            TransacaoSeeder::class,
+            // TransacaoSeeder::class,
         ]);
     }
 }
